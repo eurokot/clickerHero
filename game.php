aa14af9b051b37356f392cf8e8840a7c5f0629f0
eurokot <eurokot2016@mail.ru>
2020-04-22 20:03:30 +0300

    add game interface

--- game.php
+++ game.php
@@ -1,18 +1,23 @@
 <html>
 <head>
-	<title>Вход</title>
-	<link rel="stylesheet" type="text/css" href="css/game.css">
+    <title>Игра</title>
+    <link rel="stylesheet" type="text/css" href="css/game.css">
 </head>
 
 <body>
 <div class="bg" id="bg">
-	
-
-
-
+    <div class="content_game">
+        <h1>0123456789</h1><br>
+        <button id="click"></button>
+        <div class="nav">
+            <button id="logout">Выход</button>
+            <button id="info">Инфо</button>
+            <button id="magazine">Магазин</button>
+        </div>
+    </div>
 </div>
 <script src="js/three.r95.min.js"></script>
 <script src="js/vanta.cells.min.js"></script>
-<script src="js/bg.js"></script>	
+<script src="js/bg.js"></script>    
 </body>
 </html>
